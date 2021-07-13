76faca1400ab3c9b6381bcf8612649e1cc437aad
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-13 22:18:21 +0800

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const socket = require('socket.io')
+
+// App setup
+const PORT = 5000
+const app = express()
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+  console.log(`http://localhost:${PORT}`)
+})
+
+// Static files
+app.use(express.static('public'))
+
+// Socket setup
+const io = socket(server)
+
+io.on('connection', (socket) => {
+  console.log('Made socket connection')
+})
