19dc68900da1152998e31dfbf964644372efa8d6
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-13 22:49:15 +0800

    feat: add socket listeners in client side

--- index.js
+++ index.js
@@ -16,25 +16,25 @@ app.use(express.static('public'))
 const io = socket(server)
 
 const activeUsers = new Set()
-//sever socket 連線狀態
+// sever socket 連線狀態
 io.on('connection', (socket) => {
-  //socket 聊天室已連線
+  // socket 聊天室已連線
   console.log('Made socket connection')
-  //聊天室有新使用者進入
+  // 聊天室有新使用者進入
   socket.on('new user', (data) => {
-    //取得該使用者資料
+    // 取得該使用者資料
     socket.userId = data
-    //線上使用者列表加入該名使用者
+    // 線上使用者列表加入該名使用者
     activeUsers.add(data)
-    //聊天室通知新使用者上線
+    // 聊天室通知新使用者上線
     io.emit('new user', [...activeUsers])
   })
 
-  //偵測離線狀態
+  // 偵測離線狀態
   socket.on('disconnect', () => {
-    //線上使用者列表移除離線使用者資料
+    // 線上使用者列表移除離線使用者資料
     activeUsers.delete(socket.userId)
-    //聊天室通知該名使用者離開聊天
+    // 聊天室通知該名使用者離開聊天
     io.emit('user disconnected', socket.userId)
   })
 })
